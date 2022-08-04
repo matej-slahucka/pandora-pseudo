85690d5bad9d56efae6d39e971112be37b5cb154
Milan Suk <Milansuk@email.cz>
2022-08-04 19:44:42 +0200

    feat: pseudo-implementation of EmailSelector

--- src/pnadora/email_selector.py
+++ src/pnadora/email_selector.py
@@ -1,9 +1,64 @@
 from abc import ABC, abstractmethod
 
-from .models import Email, GetEmailRequest
+from .logger import Logger
+from .models import Email, GetEmailRequest, Rule
+from .repositories import RuleRepository
+
+
+class RuleNotFoundError(Exception):
+    pass
 
 
 class EmailSelector(ABC):
+    def __init__(self, rule_repository: RuleRepository, logger: Logger) -> None:
+        self._rule_repository = rule_repository
+        self._logger = logger
+
+    def select_email(self, request: GetEmailRequest) -> Email | None:
+        rules = self._rule_repository.get_rules(request.channel, request.carrier)
+
+        if not rules:
+            self._logger.warn("No rules matched")
+            raise RuleNotFoundError
+
+        email = None
+
+        for rule in rules:
+            email = self._get_email_by_rule(rule, request)
+
+            if email is not None:
+                break
+
+        return email
+
+    def _get_email_by_rule(self, rule: Rule, request: GetEmailRequest) -> Email | None:
+        emails_with_usage = self._rule_repository.get_emails_by_usage(request, rule)
+
+        if not emails_with_usage:
+            self._logger.warn("No domains for rule", rule=rule.name)
+            return None
+
+        email_with_usage = emails_with_usage[0]
+        if email_with_usage.frequency >= rule.frequency:
+            self._logger.warn("All domains for rule used", rule=rule.name)
+            return None
+
+        # TODO: abstract away email from account vs email from domains using the generator
+
+        self._logger.info(
+            "Email selected",
+            rule=rule.name,
+            email_name=email_with_usage.email.domain,
+            email_domain=email_with_usage.email.domain,
+        )
+
+        email = self._create_email(email_with_usage.email)
+        self._rule_repository.save_usage(email, request)
+
+        return email
+
     @abstractmethod
-    def select_email(self, request: GetEmailRequest) -> Email:
+    def _create_email(self, email: Email) -> Email:
+        # TODO: now this method doesn't really make sence because whole Email
+        # structure won't be available in general.
         ...
--- src/pnadora/repositories/rule_repository.py
+++ src/pnadora/repositories/rule_repository.py
@@ -1,9 +1,33 @@
 from abc import ABC, abstractmethod
 
-from ..models import Rule, Tier
+from ..models import (
+    Carrier,
+    Channel,
+    Email,
+    EmailWithUsage,
+    GetEmailRequest,
+    Rule,
+    StoredEmail,
+)
 
 
 class RuleRepository(ABC):
     @abstractmethod
-    def get_rules(self, tier: Tier) -> list[Rule]:
+    def find_emails(self, emails: list[Email]) -> list[StoredEmail]:
+        ...
+
+    @abstractmethod
+    def get_rules(self, channel: Channel, carrier: Carrier) -> list[Rule]:
+        ...
+
+    @abstractmethod
+    def get_emails_by_usage(
+        self, request: GetEmailRequest, rule: Rule
+    ) -> list[EmailWithUsage]:
+        ...
+
+    @abstractmethod
+    def save_usage(
+        self, email: Email, request: GetEmailRequest
+    ) -> list[EmailWithUsage]:
         ...
